8ba232cc2d16959859aa1fe012fcd2d82731bc0b
Ankit Kanaujiya <46132254+kanaujiya@users.noreply.github.com>
2025-11-09 18:21:22 +0530

    second commit

--- src/server.js
+++ src/server.js
@@ -43,7 +43,7 @@ app.use(
 // CORS
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://snm-medical.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
